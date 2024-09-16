5a9d01e638702d56f1065ba3855d5734cfa292bd
Andrei Astakhov aka Progmech <andrei.i.astakhov@gmail.com>
2024-09-16 17:42:14 +0300

    ДЗ2 Модуль 4.10 Строки и функции

--- 1-converter/main.go
+++ 1-converter/main.go
@@ -3,8 +3,21 @@ package main
 import "fmt"
 
 func main() {
-	const usdToEur = 0.9029
-	const usdToRub = 90.1361 // 99.7572
+	usdToEur, usdToRub := getUserInput()
 	eurToRub := (1.0 / usdToEur) * usdToRub
 	fmt.Println(eurToRub)
+}
+
+func getUserInput() (float64, float64) {
+	var usdToEur float64
+	var usdToRub float64
+	fmt.Print("Введите курс USD к EUR: ")
+	fmt.Scan(&usdToEur)
+	fmt.Print("Введите курс USD к RUB: : ")
+	fmt.Scan(&usdToRub)
+	return usdToEur, usdToRub
+}
+
+func convert(val int, fromCurr string, toCurr string) {
+	return
 }
