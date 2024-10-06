6fe5d42ac1da3796e0f7ac37d7a3c9ec5ba68bbe
Andrey Astakhov aka Progmech <progmech@progmech.ru>
2024-10-06 12:56:27 +0300

    Д42 Модуль 11.15 Работа с ОС

--- 3-json-loader/bins/bins.go
+++ 3-json-loader/bins/bins.go
@@ -3,20 +3,20 @@ package bins
 import "time"
 
 type Bin struct {
-	id        string
-	private   bool
-	createdAt time.Time
-	name      string
+	Id        string    `json:"id"`
+	Private   bool      `json:"private"`
+	CreatedAt time.Time `json:"createdAt"`
+	Name      string    `json:"name"`
 }
 
 type BinList []Bin
 
-func newBin(id string, private bool, name string) *Bin {
+func NewBin(id string, private bool, name string) *Bin {
 	res := &Bin{
-		id:        id,
-		private:   private,
-		createdAt: time.Now(),
-		name:      name,
+		Id:        id,
+		Private:   private,
+		CreatedAt: time.Now(),
+		Name:      name,
 	}
 	return res
 }
--- 3-json-loader/main.go
+++ 3-json-loader/main.go
@@ -1,9 +1,14 @@
 package main
 
 import (
-	"fmt"
+	"json-loader/bins"
+	"json-loader/storage"
 )
 
 func main() {
-	fmt.Println("bins")
+	var binList bins.BinList = bins.BinList{}
+	bin := bins.NewBin("a", true, "Test1")
+	binList = append(binList, *bin)
+	storage.SaveBins(&binList)
+	storage.PrintBins(&binList)
 }
